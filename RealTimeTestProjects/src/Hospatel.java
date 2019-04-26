fe6334da016d1fcd22b4b0a83fc43ec98b1a073f
mkarnam <mkarnam@MH23.corp.miraclesoft.com>
2019-04-26 16:33:33 +0530

    Hospital program committed to the repository

---
+++ RealTimeTestProjects/src/Hospatel.java
@@ -0,0 +1,43 @@
+
+public class Hospatel {
+	
+	int pid;
+	String pname;
+	String pprob;
+	
+	Hospatel(int id,String name,String prob){
+		
+		pid=id;
+		pname=name;
+		pprob=prob;
+		
+		
+	}
+	
+	void display() {
+		
+		System.out.println("Patient Details:"+pid+" "+pname+" "+pprob);
+		
+		
+	}
+	public static void main(String[] args) {
+		
+		 Hospatel h=new  Hospatel(10,"ramki","lowbp");
+		 Hospatel h1=new  Hospatel(11,"siva","coupgh");
+		 Hospatel h2=new  Hospatel(12,"santosh","mentally stresd");
+		 Hospatel h3=new  Hospatel(13,"kumar","thyroid");
+		 
+		 
+		 h.display();
+		 h1.display();
+		 h2.display();
+		 h3.display();
+		
+		
+	}
+		
+		
+	}
+	
+
+
