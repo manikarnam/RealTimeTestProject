ec0c8f8f1dd2bee091863c2eb0d0103b0cb01d39
manikarnam <49348793+manikarnam@users.noreply.github.com>
2019-04-26 16:48:10 +0530

    Update Hospatel.java
    changed

--- RealTimeTestProjects/src/Hospatel.java
+++ RealTimeTestProjects/src/Hospatel.java
@@ -17,9 +17,9 @@ System.out.println("Patient Details:"+pid+" "+pname+" "+pprob);
 		}
 	public static void main(String[] args) {
 		
-		 Hospatel h=new  Hospatel(10,"ramki","lowbp");
-		 Hospatel h1=new  Hospatel(11,"siva","coupgh");
-		 Hospatel h2=new  Hospatel(12,"santosh","mentally stresd");
+		 Hospatel h=new  Hospatel(10,"ramki","low bp");
+		 Hospatel h1=new  Hospatel(11,"siva","cough");
+		 Hospatel h2=new  Hospatel(12,"santosh","mentally stressd");
 		 Hospatel h3=new  Hospatel(13,"kumar","thyroid");
 		 
 		 
